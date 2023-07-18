600275ef3c5eae11f7940386ab5c41dc3d864ab2
tengahcang <cangg522@gmail.com>
2023-07-18 19:16:19 +0700

    database dan seeder awal

--- database/migrations/2023_07_18_120927_create_peminjamans_table.php
+++ database/migrations/2023_07_18_120927_create_peminjamans_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('peminjamans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->string('event');
+            $table->text('keterangan')->nullable();
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali');
+            $table->enum('status',['waiting','approved','decline']);
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('peminjamans');
     }
 };
